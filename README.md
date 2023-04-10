5bd76a9c61391e66a6c1c5c99c3ea3355e652ab1
Apoorv Chincholkar <apoorv_chincholkar@altizon.com>
2023-04-10 14:53:13 +0530

    Upgraded manifest to V3, and some additional improvements in the code.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# mint Preview
+# DF Preview
 ### Steps to install
-1. Download and extract the zip.
-2. In Chrome go to Settings > More tools > Extensions
-3. Enable the developer mode (top right).
-4. In Load Unpacked option, open the above unzipped folder.
+1. Download the zip file.
+2. Extract the files to a location on your computer.
+3. Open Google Chrome and click the three dots icon in the top-right corner.
+4. Select "More Tools" from the dropdown menu, then click "Extensions".
+5. In the Extensions page, enable the "Developer mode" toggle in the top-right corner.
+6. Click the "Load unpacked" button and select the folder containing the extracted extension files.
+7. The extension should now be installed and ready to use.
--- background.js
+++ background.js
@@ -1,71 +1,51 @@
 'use strict';
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        // That fires when a page's URL contains 'datonis.io'
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'datonis.io' },
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [ new chrome.declarativeContent.ShowPageAction()]
-      }
-    ]);
+
+// Add an event listener for when the extension is installed or updated
+chrome.runtime.onInstalled.addListener(() => {
+  // Remove any existing rules and add a new rule to show the page action
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      // Show the page action when the URL of the page contains "datonis.io"
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { urlContains: 'datonis.io' },
+        })
+      ],
+      // Show the extension's page action
+      actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    }]);
   });
 });
 
-function forcePreview(){
-
-chrome.tabs.query({ active: true, currentWindow: true}, function (tabs) {
-if (tabs[0] && tabs[0].url)
-{
-   let additionalParam = "preview=true";
-    let currentUrl =  new URL(tabs[0].url)
-    let urlParams = currentUrl.search
-    if(currentUrl.href.search("preview=true") >=0){
-	return}
-    if(urlParams.length > 0){
-        chrome.tabs.update(tabs[0].id, {url: currentUrl + "&" + additionalParam});	
-	}
-	else{
-	    chrome.tabs.update(tabs[0].id, {url: currentUrl + "?" + additionalParam});
-	}
-
+// Enable or disable the page action icon depending on the URL of the active tab
+function updatePageAction(tabId) {
+  chrome.tabs.get(tabId, (tab) => {
+    if (tab && tab.url && tab.url.includes('datonis.io')) {
+      chrome.action.enable(tabId);
+      chrome.action.setIcon({ path: 'assets/preview_enabled-32.png' });
+    } else {
+      chrome.action.disable(tabId);
+      chrome.action.setIcon({ path: 'assets/preview_disabled-32.png' });
+    }
+  });
 }
 
+// Listen for tab updates and update the page action icon accordingly
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'loading') {
+    updatePageAction(tabId);
+  }
 });
-}
 
-function enableBrowserAction(tabId, changeInfo, tabInfo){
-    chrome.tabs.get(tabId, function (tab){
-	if (tab.url.search("datonis.io") >= 0){
-  chrome.browserAction.enable(tab.id);
-	chrome.browserAction.setIcon({path: "assets/preview_enabled-32.png"});
-	}else{
-       chrome.browserAction.setIcon({path: "assets/preview_disabled-32.png"});
-	     chrome.browserAction.disable(tab.id);	
-	}
-	})
-}
-
-/*
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-
-    chrome.tabs.get(activeInfo.tabId, function (tab){
-	if (tab.url.search("datonis.io") >= 0){
-  chrome.browserAction.enable(tab.id);
-	chrome.browserAction.setIcon({path: "assets/preview_enabled-32.png"});
-	}else{
-       chrome.browserAction.setIcon({path: "assets/preview_disabled-32.png"});
-	     chrome.browserAction.disable(tab.id);	
-	}
-	})
+// Add an event listener for when the page action button is clicked
+chrome.action.onClicked.addListener((tab) => {
+  // If the tab's URL already contains "preview=true", do nothing
+  if (tab.url.includes('preview=true')) {
+    return;
+  }
+  // Get the current URL and add the "preview=true" parameter
+  let url = new URL(tab.url);
+  url.searchParams.set('preview', 'true');
+  // Update the tab's URL with the new parameter
+  chrome.tabs.update(tab.id, { url: url.href });
 });
-*/
-
-chrome.tabs.onUpdated.addListener(enableBrowserAction)
-
-chrome.browserAction.onClicked.addListener(forcePreview)
